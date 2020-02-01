cf548688eafa2d36498baf457064efa575220dcd
reahaas <rea@scadafence.com>
2020-02-01 12:33:24 +0200

    gggg

--- Assets/Scripts/CitizenGenerator.cs
+++ Assets/Scripts/CitizenGenerator.cs
@@ -3,17 +3,22 @@
 public class CitizenGenerator : MonoBehaviour
 {
     public GameObject myPrefab;
+    public GameObject lookAt;
     public int citizentsCount = 4;
     public int randomRange = 5;
 
 
     void Start()
     {
-        for (float i = 0; i < citizentsCount; i+=2)
+        for (float i = 0; i < citizentsCount; i += 2)
         {
-            for (float j = 0; j < citizentsCount; j+=2)
+            for (float j = 0; j < citizentsCount; j += 2)
             {
-                Instantiate(myPrefab, new Vector3(transform.position.x + i + Random.Range(0f, 1f), transform.position.y, transform.position.z + j + Random.Range(0f, 1f)), Quaternion.identity);
+                PenguinMovment temp = Instantiate(myPrefab,
+                    new Vector3(transform.position.x + i + Random.Range(0f, 1f), transform.position.y, transform.position.z + j + Random.Range(0f, 1f)), 
+                    Quaternion.identity).GetComponent<PenguinMovment>();
+                temp.SetLookAt(lookAt);
+
             }
         }
     }
--- Assets/Scripts/PenguinMovment.cs
+++ Assets/Scripts/PenguinMovment.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 
 public class PenguinMovment : MonoBehaviour
 {
+    public GameObject penguin;
     public float speed = 10f;
+    private GameObject _lookAt;
+    private Boolean isMoveToCenter = false;
+    private float movmentInterval = 5;
+    private float time = 0;
+
+    public void SetLookAt(GameObject lookAt)
+    {
+        _lookAt = lookAt;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,20 +34,59 @@ public class PenguinMovment : MonoBehaviour
 
     void ControllPenguin()
     {
-        
-        int smallRotate = UnityEngine.Random.Range(-1, 2);
+        time += Time.deltaTime;
 
+        if (time > movmentInterval)
+        {
+            isMoveToCenter = !isMoveToCenter;
+            time = UnityEngine.Random.Range(3f, 5f);
+
+        }
+
+        float added = 0;
+        float returnHomeBios = Vector3.Distance(transform.position, Vector3.zero);
+        if (returnHomeBios > 15)
+        {
+            added = 1;
+        }
+        float smallRotate = UnityEngine.Random.Range(-1, 2 + added);
+
+
+        if (isMoveToCenter)
+        {
+            smallRotate -= 1;
+        }
+        else
+        {
+            smallRotate += 1;
+        }
         transform.Rotate(0, smallRotate, 0);
-
         transform.Translate(Vector3.left * Time.deltaTime);
 
+        // showDegubLines();
 
+    }
 
+    private void showDegubLines()
+    {
+        Vector3 targetDirection = _lookAt.transform.position - transform.position;
         float singleStep = speed * Time.deltaTime;
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, Vector3.zero, singleStep, 0f);
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        newDirection = transform.forward;
+        Debug.DrawRay(transform.position, newDirection, Color.red);
 
-        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
 
+    private float getTurnEngle(float y)
+    {
+        if (y >= 180)
+        {
+            return 1f;
+        }
+        else
+        {
+            return -1f;
+        }
     }
 
 
