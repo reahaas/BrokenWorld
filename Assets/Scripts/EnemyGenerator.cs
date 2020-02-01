6f415c34cf24b2b7fb07e8e5a90ef07b52b330a5
Guy Kroizman <kroizbox@gmail.com>
2020-02-01 02:41:51 +0200

    tighter enemies

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -4,16 +4,16 @@ public class EnemyGenerator : MonoBehaviour
 {
     public GameObject myPrefab;
     public int Count = 4;
-    public int randomRange = 5;
+    public float randomRange;
 
 
     void Start()
     {
-        for (float i = 0; i < Count; i+=2)
+        for (float i = 0; i < Count; i+=0.9f)
         {
-            for (float j = 0; j < Count; j+=2)
+            for (float j = 0; j < Count; j+=0.9f)
             {
-                Instantiate(myPrefab, new Vector3(transform.position.x + i + Random.Range(0f, 1f), transform.position.y, transform.position.z + j + Random.Range(0f, 1f)), Quaternion.identity);
+                Instantiate(myPrefab, new Vector3(transform.position.x + i + Random.Range(0f, randomRange), transform.position.y, transform.position.z + j + Random.Range(0f, randomRange)), Quaternion.identity);
             }
         }
     }
