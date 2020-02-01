d5f9799b45b915dcfc512b991cbe62de5e550766
eran wass <eran.wass@oracle.com>
2020-02-01 13:48:17 +0200

    asdf

--- Assets/MonsterPenguinCollide.cs
+++ Assets/MonsterPenguinCollide.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterPenguinCollide : MonoBehaviour
 {
     public Transform explosionPrefab;
+    ArrayList badGuyNames = new ArrayList();
 
     void Start()
     {
+        badGuyNames.Add("Boximon");
+
     }
 
     // Update is called once per frame
@@ -18,13 +22,31 @@ public class MonsterPenguinCollide : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(("Collision !"));
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 position = contact.point;
-        
-        GetComponent<AudioSource>().Play();
-        Instantiate(explosionPrefab, position, rotation);
-        Destroy(gameObject);
+        String bumpIntoName = collision.collider.name;
+        if (isBadGuy(bumpIntoName))
+        {
+            print("Detected collision between " + gameObject.name + " and " + bumpIntoName);
+            print("There are " + collision.contacts.Length + " point(s) of contacts");
+            print("Their relative velocity is " + collision.relativeVelocity);
+            ContactPoint contact = collision.contacts[0];
+            Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+            Vector3 position = contact.point;
+
+            GetComponent<AudioSource>().Play();
+            Instantiate(explosionPrefab, position, rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    private bool isBadGuy(string bumpIntoName)
+    {
+        for (int i = 0; i < badGuyNames.Count; i++)
+        {
+            String badDude = badGuyNames[i].ToString();
+            if (bumpIntoName.ToLower().Contains(badDude))
+                return true;
+        }
+
+        return false;
     }
 }
