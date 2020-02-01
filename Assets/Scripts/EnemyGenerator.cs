fad1e2f6659de1abef63133b3403c533d560abf1
Guy Kroizman <kroizbox@gmail.com>
2020-02-01 02:07:15 +0200

    add enenmy

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public int Count = 4;
+    public int randomRange = 5;
+
+
+    void Start()
+    {
+        for (float i = 0; i < Count; i+=2)
+        {
+            for (float j = 0; j < Count; j+=2)
+            {
+                Instantiate(myPrefab, new Vector3(transform.position.x + i + Random.Range(0f, 1f), transform.position.y, transform.position.z + j + Random.Range(0f, 1f)), Quaternion.identity);
+            }
+        }
+    }
+
+}
