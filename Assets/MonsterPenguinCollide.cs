54a01b28914f23b3a338a5a64c4ce405c41190f6
eran wass <eran.wass@oracle.com>
2020-02-01 13:26:37 +0200

    new skymap. all penguins exploding

---
+++ Assets/MonsterPenguinCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterPenguinCollide : MonoBehaviour
+{
+    public Transform explosionPrefab;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(("Collision !"));
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 position = contact.point;
+        
+        GetComponent<AudioSource>().Play();
+        Instantiate(explosionPrefab, position, rotation);
+        Destroy(gameObject);
+    }
+}
