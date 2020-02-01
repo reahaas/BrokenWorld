12219add7382b0955cee17e1394dbabeb8690049
Guy Kroizman <kroizbox@gmail.com>
2020-02-01 19:05:39 +0200

    qiot

---
+++ Assets/Scripts/CheckGameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGameOver : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (PenguinCounter.count == 0 && Time.frameCount > 30)
+            Application.Quit();
+
+    }
+}
