2ca1043f7a679bdbb93b41cf5dd68e19a0d7bffd
Guy Kroizman <kroizbox@gmail.com>
2020-02-01 13:39:08 +0200

    penguin counters

---
+++ Assets/Scripts/DestroyPenguinIfTooFar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DestroyPenguinIfTooFar : MonoBehaviour
+{
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        
+    }
+
+
+    void Update()
+    {
+        if (Vector3.Distance(startPosition, transform.position) > 10)
+            Destroy(gameObject);
+
+    }
+
+    private void OnDestroy()
+    {
+        PenguinCounter.count -= 1;
+    }
+}
